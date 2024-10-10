e08d8ed141f7377f445b32191c11c1d9d81a415b
samorobo <samgodwin111@gmail.com>
2024-10-11 00:17:42 +0100

    Implemented the passkey OTP that grants you access to the admin dashboard

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,17 @@
+import PasskeyModal from "@/components/PasskeyModal";
 import PatientForm from "@/components/forms/PatientForm";
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
 import Link from "next/link";
 
 
-export default function Home() {
+export default function Home({ searchParams}: SearchParamProps) {
+  //checks if a user wants to log in as admin
+
+  const isAdmin = searchParams.admin === 'true';
   return (
     <div className="flex h-screen max-h-screen">
-      {/** TODO: OTP Verification | PasskeyModal */}
+      {isAdmin && <PasskeyModal />}
       <section className="remove-scrollbar container my-auto">
         <div className="sub-container max-w-[496px]">
           <Image 
