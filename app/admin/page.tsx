1bfe7441bd3a0098b623ecbe6e12ab4df271c96f
samorobo <samgodwin111@gmail.com>
2024-11-08 01:09:15 +0100

    Added some changes

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -10,7 +10,7 @@ import { columns } from '@/components/table/columns'
 
 const Admin = async () => {
    
-    const appointments = await getRecentAppointmentList()
+    const appointments = await getRecentAppointmentList() || {};
   return (
     <div className='mx-auto flex max-w-7xl flex-col space-y-14'>
     <header className='admin-header'>
@@ -25,11 +25,11 @@ const Admin = async () => {
     <p className='text-dark-700'>Start the day with managing new appointments</p>
     </section>
     <section className='admin-stat'>
-    <StatCard type="appointments" count={appointments.scheduledCount} label="Scheduled appointments" icon="/assets/icons/appointments.svg"/>
-    <StatCard type="pending" count={appointments.pendingCount} label="Pending appointments" icon="/assets/icons/pending.svg"/>
-    <StatCard type="cancelled" count={appointments.cancelledCount} label="Cancelled appointments" icon="/assets/icons/cancelled.svg"/>
+    <StatCard type="appointments" count={appointments.scheduledCount || 0} label="Scheduled appointments" icon="/assets/icons/appointments.svg"/>
+    <StatCard type="pending" count={appointments.pendingCount || 0} label="Pending appointments" icon="/assets/icons/pending.svg"/>
+    <StatCard type="cancelled" count={appointments.cancelledCount || 0} label="Cancelled appointments" icon="/assets/icons/cancelled.svg"/>
     </section>
-    <DataTable columns={columns} data={appointments.documents}/>
+    <DataTable columns={columns} data={appointments.documents || []}/>
     </main>
     </div>
   )
